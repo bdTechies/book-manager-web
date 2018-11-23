1dbd686b720ddbc148d55159f7a8b5bdfc2d71ca
sh4hids <hello@shahid.pro>
2018-11-23 21:38:25 +0600

    Fixed header and dload btn

--- js/app.js
+++ js/app.js
@@ -31,4 +31,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
       windowsBtn.className += " active-btn";
       break;
   }
+
+  linuxBtn.addEventListener("click", function() {
+    downloadBtn.href = "linux-url";
+    windowsBtn.classList.remove("active-btn");
+    macBtn.classList.remove("active-btn");
+    linuxBtn.className += " active-btn";
+  });
+
+  windowsBtn.addEventListener("click", function() {
+    downloadBtn.href = "windows-url";
+    linuxBtn.classList.remove("active-btn");
+    macBtn.classList.remove("active-btn");
+    windowsBtn.className += " active-btn";
+  });
+
+  macBtn.addEventListener("click", function() {
+    downloadBtn.href = "mac-url";
+    linuxBtn.classList.remove("active-btn");
+    windowsBtn.classList.remove("active-btn");
+    macBtn.className += " active-btn";
+  });
 });

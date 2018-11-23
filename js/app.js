78852bb43f7cf43687a23b1bbef2710da04db609
sh4hids <hello@shahid.pro>
2018-11-23 19:24:24 +0600

    Added platform info

---
+++ js/app.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log(platform.description);
+  var os = platform.os.family;
+  var linuxBtn = document.querySelector(".linux-os");
+  var downloadBtn = document.querySelector(".download-btn");
+  var windowsBtn = document.querySelector(".windows-os");
+  var macBtn = document.querySelector(".mac-os");
+  switch (os) {
+    case os.indexOf("Linux") !== -1:
+      downloadBtn.href = "linux-url";
+      windowsBtn.classList.remove("active-btn");
+      macBtn.classList.remove("active-btn");
+      linuxBtn.className += " active-btn";
+      break;
+    case os.indexOf("Win") !== -1:
+      downloadBtn.href = "windows-url";
+      linuxBtn.classList.remove("active-btn");
+      macBtn.classList.remove("active-btn");
+      windowsBtn.className += " active-btn";
+      break;
+    case os.indexOf("Mac") !== -1:
+      downloadBtn.href = "mac-url";
+      linuxBtn.classList.remove("active-btn");
+      windowsBtn.classList.remove("active-btn");
+      macBtn.className += " active-btn";
+      break;
+    default:
+      linuxBtn.classList.remove("active-btn");
+      macBtn.classList.remove("active-btn");
+      windowsBtn.className += " active-btn";
+  }
+});
